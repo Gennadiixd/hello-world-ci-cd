57c6d136f6def54f7432ce7e7e125070e7b1f17b
Gennadiixd <gennadiixd@gmail.com>
2020-06-15 16:06:11 +0300

    configure husky, jest

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
-
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
 
 module.exports = { app };
--- app.test.js
+++ app.test.js
@@ -1,10 +1,24 @@
 const request = require("supertest");
 const { app } = require("./app");
 
+beforeEach((done) => {
+  server = app.listen(4000, (err) => {
+    if (err) return done(err);
+
+    agent = request.agent(server);
+    done();
+  });
+});
+
+afterEach((done) => {
+  return server && server.close(done);
+});
+
 describe("App", () => {
-  it('should respond "Hello World" ', async () => {
-    const res = await request(app).get("/").send("Hello World");
+  it('should respond "Hello World" ', async (done) => {
+    const res = await request(app).get("/");
     expect(res.statusCode).toEqual(200);
     expect(res.text).toBe("Hello World!");
+    done();
   });
 });
