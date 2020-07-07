3184544632c49bda0347f5c94b630f479981d29c
Gennadiixd <gennadiixd@gmail.com>
2020-07-07 16:27:47 +0300

    ssr cert

--- app/services/core/http-service.ts
+++ app/services/core/http-service.ts
@@ -6,7 +6,7 @@ export default class HttpService {
     this.options = options;
   }
 
-  BASE_URL = "myprojectdomain.dev:3000";
+  BASE_URL = "myprojectdomain.dev:3080";
 
   async get(endpoint, options = {}): Promise<{ data: any }> {
     return axios.get(`${this.BASE_URL}/${endpoint}`, options);
