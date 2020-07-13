84969b265b6a9edf8bcd5f88a933c26061a6457a
Gennadiixd <gennadiixd@gmail.com>
2020-07-13 14:07:51 +0300

    fix env

--- api/connection.ts
+++ api/connection.ts
@@ -2,7 +2,7 @@ import { createConnection } from "typeorm";
 
 export const connection = createConnection({
   type: "postgres",
-  host: process.env.DB_HOSTNAME,
+  host: process.env.DB_HOST,
   port: +process.env.DB_PORT,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
