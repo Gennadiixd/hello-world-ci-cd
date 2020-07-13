8a1516de2db82fb687923b928ea68333524432f5
Gennadiixd <gennadiixd@gmail.com>
2020-07-13 20:50:33 +0300

    try

--- api/app.test.ts
+++ api/app.test.ts
@@ -14,9 +14,6 @@ container.register("IProductsRepository", {
 });
 
 describe("App", () => {
-  process.on('unhandledRejection', (reason, promise) => {
-    console.log('Unhandled Rejection at:', reason)
-  });
   it("should return success", async (done) => {
     const productsService = container.resolve(ProductsService);
     expect(productsService.getProducts()).toBe("success");
