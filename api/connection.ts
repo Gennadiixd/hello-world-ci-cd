8c4a338b824b62cf3ae5d82b08f39b7f4478578c
Gennadiixd <gennadiixd@gmail.com>
2020-07-14 14:55:07 +0300

    refactor

--- api/connection.ts
+++ api/connection.ts
@@ -1,43 +1,26 @@
 import { createConnection, Connection } from "typeorm";
 import { injectable } from "tsyringe";
 
-interface IMockConnection {
-  manager: {
-    find: () => any;
-  };
-}
-
-const mockConnection: Promise<IMockConnection> = Promise.resolve({
-  manager: {
-    find: () => {
-      return [1, 2, 3, 5];
-    },
-  },
-});
-
 export interface IDBConnection {
-  connection: Promise<Connection> | Promise<IMockConnection>;
-  getConnection: () => Promise<Connection> | Promise<IMockConnection>;
+  connection: Promise<Connection>;
+  getConnection: () => Promise<Connection>;
 }
 
 @injectable()
 class DBConnection implements IDBConnection {
-  connection: Promise<Connection> | Promise<IMockConnection>;
+  connection: Promise<Connection>;
 
   constructor() {
-    this.connection =
-      process.env.NODE_ENV === "test"
-        ? mockConnection
-        : createConnection({
-            type: "postgres",
-            host: process.env.DB_HOST,
-            port: +process.env.DB_PORT,
-            username: process.env.DB_USERNAME,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
-            entities: [__dirname + "/components/**/*.entity{.ts,.js}"],
-            synchronize: false
-          });
+    this.connection = createConnection({
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + "/components/**/*.entity{.ts,.js}"],
+      synchronize: false,
+    });
   }
 
   getConnection() {
