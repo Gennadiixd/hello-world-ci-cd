ec854bedea9047781a262c846a2c42c42c55b6c7
Gennadiixd <gennadiixd@gmail.com>
2020-07-29 13:58:12 +0300

    www

--- api/db/seeds/02_users.ts
+++ api/db/seeds/02_users.ts
@@ -1,5 +1,5 @@
 import * as Knex from "knex";
-// import { hashSync } from "bcrypt";
+import { hashSync } from "bcrypt";
 
 const SALT_ROUNDS = 10;
 
@@ -9,14 +9,12 @@ export async function seed(knex: Knex): Promise<void> {
   await knex("users").insert([
     {
       name: "userOne",
-      password: "1234",
-      // password: hashSync("1234", SALT_ROUNDS),
+      password: hashSync("1234", SALT_ROUNDS),
       role: "customer",
     },
     {
       name: "admin",
-      password: "1234",
-      // password: hashSync("1234", SALT_ROUNDS),
+      password: hashSync("1234", SALT_ROUNDS),
       role: "admin",
     },
   ]);
