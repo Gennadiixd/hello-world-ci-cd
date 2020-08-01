5e0627b04008a921d7655aeac325a85acb4fd4da
Gennadiixd <gennadiixd@gmail.com>
2020-08-01 19:26:03 +0300

    refactor protected routes logic

--- app/components/security/admin-route.tsx
+++ app/components/security/admin-route.tsx
@@ -1,24 +1,9 @@
-import Router from "next/router";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { setCurrentUserAC } from "@/views/user/ducks/action-creators";
-
-import useClaims from "./use-claims";
+import useAllowed from "./use-allowed";
 
 export default function AdminRoute(Component) {
   return (...args) => {
-    const dispatch = useDispatch();
-    const claims = useClaims();
-    const handleUserLogout = () => {
-      dispatch(setCurrentUserAC(null));
-    };
 
-    useEffect(() => {
-      if (claims?.role !== "admin") {
-        Router.push("/user");
-        handleUserLogout();
-      }
-    }, [claims]);
+    useAllowed({ forRole: "admin", redirectTo: "user" });
 
     return <Component {...args} />;
   };
---
+++ app/components/security/use-allowed.tsx
@@ -0,0 +1,24 @@
+import React, { useLayoutEffect } from "react";
+import { useDispatch } from "react-redux";
+import useClaims from "./use-claims";
+import { setCurrentUserAC } from "../../views/user/ducks";
+import Router from "next/router";
+
+export default function useAllowed({ forRole, redirectTo }) {
+  const dispatch = useDispatch();
+  const claims = useClaims();
+  const handleUserLogout = () => {
+    dispatch(setCurrentUserAC(null));
+  };
+
+  useLayoutEffect(() => {
+    if (!claims) {
+      handleUserLogout();
+    }
+    if (claims?.role !== forRole) {
+      Router.push(`/${redirectTo}`);
+    }
+  }, [claims]);
+
+  return <div></div>;
+}
