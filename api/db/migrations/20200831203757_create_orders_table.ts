b33c15d98889ae7db9643966c4dbff529a22302e
Gennadiixd <gennadiixd@gmail.com>
2020-08-31 23:55:32 +0300

    refs

--- api/db/migrations/20200831203757_create_orders_table.ts
+++ api/db/migrations/20200831203757_create_orders_table.ts
@@ -4,10 +4,12 @@ export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTableIfNotExists("orders", function (table) {
     table.increments("id").primary();
     table.integer("price");
+    
     table.integer("user_id");
     table.foreign("user_id").references("id").inTable("users");
-    table.specificType("products_id", "integer ARRAY");
-    table.foreign("user_id").references("id").inTable("products");
+
+    table.specificType("product_ids", "INT[]");
+    table.foreign("product_ids").references("id").inTable("products");
   });
 }
 
